a96ee81080f38328b8cbbedc43f5c38036afdf8e
Jordan PAULINE <jpauline@e3r8p20.clusters.42paris.fr>
2023-01-30 18:34:08 +0100

    enable_if, is_integral, iterator_traits, random_access_iterator

---
+++ random_access_iterator.hpp
@@ -0,0 +1,176 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   random_access_iterator.hpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpauline <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 17:28:16 by jpauline          #+#    #+#             */
+/*   Updated: 2023/01/30 18:33:00 by jpauline         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RANDOM_ACCESS_ITERATOR_HPP
+#define RANDOM_ACCESS_ITERATOR_HPP
+
+namespace ft
+{
+
+	template<typename T>
+	class random_access_iterator
+	{
+		private :
+
+			pointer	_ptr;
+
+		public :
+
+			typedef typename ft::random_access_iterator_tag	iterator_category;
+			typedef std::ptrdiff_t							difference_type;
+			typedef T										value_type;
+			typedef size_t									size_type;
+			typedef T*										pointer;
+			typedef T&										reference;
+			typedef const T*								const_pointer;
+			typedef const T&								const_reference;
+
+		/*COPLIEN*/
+
+			random_access_iterator(void) : _ptr(NULL) {}
+			random_access_iterator(pointer _ptr) : _ptr(_ptr) {}
+			random_access_iterator(const random_access_iterator<T> & src) : _ptr(src.base()) {}
+			~random_access_iterator(void) {}
+
+			random_access_iterator & operator=(random_access_iterator<T> const & rhs)
+			{
+				this->_ptr = rhs.base();
+				return *this;
+			}
+			
+			/* Potentiellement necessaire pour copier un const iterator a partir d'un iterator
+			operator random_access_iterator<T const>() const
+			{ return random_access_iterator<T const>(this->_ptr); }
+			*/
+
+			//Le getteur s'appelle 'base' pour les iterators
+			pointer base() const
+			{ return this->_ptr; }
+
+
+		/*Liste des surcharges a assurer : https://cplusplus.com/reference/iterator/RandomAccessIterator/ */
+
+			bool operator==(const random_access_iterator & src) const
+			{ return (this->_ptr == src.base()); }
+
+			bool operator!=(const random_access_iterator & src) const
+			{ return !(operator==(src)); }
+
+			bool operator>(const random_access_iterator & src) const
+			{ return (this->_ptr > src.base()); }
+			
+			bool operator>=(const random_access_iterator & src) const
+			{ return (this->_ptr >= src.base()); }
+			
+			bool operator<(const random_access_iterator & src) const
+			{ return (this->_ptr < src.base()); }
+			
+			bool operator<=(const random_access_iterator & src) const
+			{ return (this->_ptr <= src.base()); }
+
+			pointer operator->(void) 
+			{ return (this->_ptr); }
+
+			reference operator*(void)
+			{ return (*this->_ptr); }
+			
+			random_access_iterator & operator++(void) 
+			{
+				this->_ptr++;
+				return *this;
+			}
+
+			random_access_iterator operator++(int)
+			{
+				random_access_iterator tmp = *this;
+				++this->_ptr;
+				return tmp;
+			}
+
+			random_access_iterator & operator--(void)
+			{
+				--this->_ptr;
+				return *this;
+			}
+
+			random_access_iterator operator--(int)
+			{
+				random_access_iterator tmp = *this;
+				--this->_ptr;
+				return tmp;
+			}
+
+			random_access_iterator operator+(difference_type n) const
+			{ return (this->_ptr + n); }
+
+			difference_type operator+(const random_access_iterator & rhs) const
+			{ return (this->_ptr + rhs.base()); }
+
+			random_access_iterator operator-(difference_type n) const
+			{ return (this->_ptr - n); }
+
+			difference_type operator-(const random_access_iterator & rhs) const
+			{ return (this->_ptr - rhs.base()); }
+
+			random_access_iterator & operator+=(difference_type n)
+			{
+				this->_ptr += n;
+				return *this;
+			}
+
+			random_access_iterator & operator-=(difference_type n)
+			{
+				this->_ptr -= n;
+				return *this;
+			}
+	
+			reference operator[](difference_type n) const
+			{ return *(*this + n); }
+			
+	};
+
+
+		template <class T>
+		random_access_iterator<T> operator+(typename random_access_iterator<T>::difference_type n, const random_access_iterator<T> &rhs)
+		{ return rhs + n; }
+
+		template <typename Ite1, typename Ite2>
+		typename ft::random_access_iterator<T>::difference_type operator-(const random_access_iterator<Ite1> &lhs, const random_access_iterator<Ite2> &rhs)
+		{ return (lhs.base() - rhs.base()); }
+
+		template <typename Ite1, typename Ite2>
+		bool operator==(const random_access_iterator<Ite1> &lhs, const random_access_iterator<Ite2> &rhs)
+		{ return (lhs.base() == rhs.base()); }
+
+		template <typename Ite1, typename Ite2>
+		bool operator!=(const random_access_iterator<Ite1> &lhs, const random_access_iterator<Ite2> &rhs)
+		{ return (lhs.base() != rhs.base()); }
+
+		template <typename Ite1, typename Ite2>
+		bool operator<(const random_access_iterator<Ite1> &lhs, const random_access_iterator<Ite2> &rhs)
+		{ return (lhs.base() < rhs.base()); }
+
+		template <typename Ite1, typename Ite2>
+		bool operator>(const random_access_iterator<Ite1> &lhs, const random_access_iterator<Ite2> &rhs)
+		{ return (lhs.base() > rhs.base()); }
+
+		template <typename Ite1, typename Ite2>
+		bool operator>=(const random_access_iterator<Ite1> &lhs, const random_access_iterator<Ite2> &rhs)
+		{ return (lhs.base() >= rhs.base()); }
+
+		template <typename Ite1, typename Ite2>
+		bool operator<=(const random_access_iterator<Ite1> &lhs, const random_access_iterator<Ite2> &rhs)
+		{ return (lhs.base() <= rhs.base()); }
+
+}
+
+#endif
---
+++ vector.hpp
@@ -0,0 +1,102 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+
+namespace ft
+{
+
+
+
+/*
+MEMBER TYPES
+~~~~~~~~~~~~
+value_type
+allocator_type
+reference
+const_reference
+pointer
+const_pointer
+iterator
+const_iterator
+reverse_iterator
+const_reverse_iterator
+difference_type
+size_type
+*/
+
+/*
+MEMBER FUNCTIONS
+~~~~~~~~~~~~~~~~
+(constructor)
+(destructor)
+operator= (assign content)
+*/
+
+/*
+Iterators:
+~~~~~~~~~~
+begin
+end
+rbegin
+rend
+cbegin (c++11)
+cendy (c++11)
+crbegin (c++11)
+crend (c++11)
+*/
+
+/*
+Capacity:
+~~~~~~~~~
+size
+max_size
+resize
+capactity
+empty
+reserve
+shrink_to_fit (c++11)
+*/
+
+/*
+Element access:
+~~~~~~~~~~~~~~~
+operator[]
+at
+front
+back
+data
+*/
+
+/*
+Modifiers:
+~~~~~~~~~~
+assign
+push_back
+pop_back
+insert
+erase
+swap
+clear
+emplace (c++11)
+emplace_back (c++11)
+*/
+
+/*
+Allocator:
+~~~~~~~~~~
+get_allocator
+*/
+
+/*
+Non-member function overloads:
+relational operators
+swap
+*/
+	
+}
+
+
+
+
+
+#endif
